c748bb750d5f71e55f89a839b8f3c30b8041da4a
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-01-24 20:43:32 +0530

    Create sol.cpp

---
+++ 909-Snakes and Ladders/sol.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+pair<int,int> coordinates(int x,int n){
+    int r=n - (x-1) / n-1;
+    int c=(x-1)%n;
+    if(r%2 == n%2)
+        return {r,n-c-1};
+    return {r,c};
+}
+    int snakesAndLadders(vector<vector<int>>& board) {
+        // in board if -1 nothing present 
+        // else either snake or ladder so go to specified position.
+        int steps=0;
+        queue<int>q;
+        q.push(1);
+        int n= board.size();
+        vector<vector<bool>> visited(n,vector<bool>(n));
+        visited[board.size() -1][0] = true;
+
+        while(q.size()!=0){
+            int z= q.size();
+            for(int i=0;i<z;i++){
+                int x = q.front();
+                if(x==n*n)
+                    return steps;
+                
+                q.pop();
+
+                for(int j=1;j<=6;j++){ // check all possibilities.
+                    if(j+x > n*n) // if size greater than breadth then return
+                        break;
+                    pair<int,int> p = coordinates(x+j,n);
+                    if(visited[p.first][p.second])
+                        continue;
+                    visited[p.first][p.second]=true;
+                    if(board[p.first][p.second]!=-1)
+                        q.push(board[p.first][p.second]);
+                    else
+                        q.push(j+x);
+                }
+            }
+            steps++;
+        }    
+        return -1;
+            }
+};
