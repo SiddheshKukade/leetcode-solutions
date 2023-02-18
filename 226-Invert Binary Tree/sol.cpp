02f0da3edaacb07145b81a4f4188e81fdc79b3c9
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-02-18 16:19:19 +0530

    TC 0N SC none

---
+++ 226-Invert Binary Tree/sol.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void swap(TreeNode* curr){
+    if(!curr){
+        return;
+    }
+    swap(curr->left);
+    swap(curr->right);
+
+    TreeNode* temp;
+    //interchanging the pointer
+    temp = curr->left;
+    curr->left = curr->right;
+    curr->right = temp;
+}
+    TreeNode* invertTree(TreeNode* root) {
+            swap(root);
+        return root;
+    }
+};
