57a656ed6a6de6e6a5b421c24537da4dd51d457f
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-02-22 16:21:53 +0530

    Create sol.sql

---
+++ 196-Delete Duplicate Emails/sol.sql
@@ -0,0 +1,6 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+
+Delete p2 FROM Person p1 JOIN Person p2
+ON p1.email = p2.email
+AND p1.id < p2.id
