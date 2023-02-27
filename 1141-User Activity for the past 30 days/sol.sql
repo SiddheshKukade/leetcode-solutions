0edae9d2b31c2600524d2288e31e6f119862c90f
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-02-27 15:34:44 +0530

    Create sol.sql

---
+++ 1141-User Activity for the past 30 days/sol.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM activity 
+WHERE activity_date BETWEEN DATE_ADD('2019-07-27', INTERVAL -29 DAY) AND "2019-07-27"
+GROUP BY activity_date
