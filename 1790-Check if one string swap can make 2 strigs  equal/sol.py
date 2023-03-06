fd7ef68e9ba93a4d26b1c02f237b78d1969d7ef6
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-03-07 00:05:09 +0530

    Create sol.py

---
+++ 1790-Check if one string swap can make 2 strigs  equal/sol.py
@@ -0,0 +1,19 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        # save the strings in a hashmap and then check the couner
+        d1 = {}
+        d2 = {}
+
+        for i in range(len(s1)):
+            if s1[i] == s2[i]:
+                pass
+            else:
+                if s1[i] in d1:
+                    return False
+                d1[s1[i]] = s2[i]
+                d2[s2[i]] = s1[i]
+
+            if d1 == d2 and len(d1) <=2:
+                return True
+            else:
+                return False
