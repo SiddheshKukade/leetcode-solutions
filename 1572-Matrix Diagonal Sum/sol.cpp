4fe071671d245ab5b4d5fb93af439e17fb23fbfb
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-03-09 17:21:37 +0530

    Create sol.cpp

---
+++ 1572-Matrix Diagonal Sum/sol.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int sum =0;
+        for(int i=0; i<n; ++i){
+            sum  += mat[i][i] + mat[i][n-i-1];
+        }
+        // IF N OF ROWS IS ODD THEN REMOVE THAT DUPLICATE ELEMENT FROM SUM
+        if( n % 2 ) sum -= mat[n/2][n/2]; 
+        return sum ;
+    }
+};
