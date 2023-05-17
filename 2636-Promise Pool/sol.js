11b185b35a1c7728fdbd4c4e72abd624d6b9ab46
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-05-17 08:10:20 +0530

    Create sol.js

---
+++ 2636-Promise Pool/sol.js
@@ -0,0 +1,24 @@
+var promisePool = async function(functions, n) {
+  return new Promise((resolve)=>{
+      let inProgressCount =0;
+      let functionIndex = 0;
+      function helper(){
+        //   BASE CASE
+          if(functionIndex >= functions.length){
+              if(inProgressCount ==0) resolve();
+              return;
+          }
+          while(inProgressCount <n && functionIndex <functions.length){
+              inProgressCount++;
+              const promise = functions[functionIndex]();
+              functionIndex++;
+              promise.then(()=>{
+                  inProgressCount--;
+                  helper();
+              });
+          }
+      }
+      helper();
+  });
+
+};
