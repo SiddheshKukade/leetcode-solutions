39eb7a58c6108a99250dfb17996068f7b7e22cd4
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-05-23 00:05:46 +0530

    Update sol.cpp

--- 263-Ugly Number/sol.cpp
+++ 263-Ugly Number/sol.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    bool isUgly(int n) {
-        while(n>1){
-            if(n%2 ==0){
-                n = n/2;
-            }if(n%3 ==0){
-                n = n/3;
-            }if(n%5 ==0){
-                n = n/5;
-            }else{ 
-                break;
-            }
+    bool isUgly(int num) {
+        // while(n>1){
+        //     if(n%2 ==0){
+        //         n = n/2;
+        //     }if(n%3 ==0){
+        //         n = n/3;
+        //     }if(n%5 ==0){
+        //         n = n/5;
+        //     }else{ 
+        //         break;
+        //     }
+        // }
+        // return n ==1;
+           if (num <= 0) return false;
+        if (num == 1) return true;
+        if (num % 2 == 0) {
+            return isUgly(num/2);
+         }
+        if (num % 3 == 0) {
+            return isUgly(num/3);
         }
-        return n ==1;
+        if (num % 5 == 0) {
+            return isUgly(num/5);
+        }
+        return false;
     }
 };
