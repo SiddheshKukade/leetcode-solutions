1df387bff2b86a436a3d0d61660830c9491d8a3a
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-05-22 18:23:53 +0530

    Create sol.java

---
+++ 141-Linked List Cycle/sol.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+     if(head == null) return false;
+
+     ListNode slow = head;
+     ListNode fast = head.next;
+
+     while(slow != fast){
+            if(fast == null || fast.next == null){
+                return false; // no cycle
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+            
+     }   
+     return true; // cycle exists.
+    }
+}
