411224dfdabeead5dcd0d9bbf7484981127de742
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-05-22 20:35:30 +0530

    Update sol.java

--- 205. Isomorphic Strings/228-Summary Ranges/sol.java
+++ 205. Isomorphic Strings/228-Summary Ranges/sol.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> list = new ArrayList();
-        int n = nums.length();
+        int n = nums.length;
         for(int i=0; i<n;i++){
             int start = nums[i];
             while(i+1<n && nums[i+1] == nums[i] +1) // moving untll the range exists
