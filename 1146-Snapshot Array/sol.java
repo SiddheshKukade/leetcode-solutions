e4b1c5ac8e4cf88b8ee4366ea757dc74399aa882
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-06-12 19:25:23 +0530

    Create sol.java

---
+++ 1146-Snapshot Array/sol.java
@@ -0,0 +1,37 @@
+class SnapshotArray {
+    int sid =0;
+    HashMap<Integer,Integer> [] map;
+
+    public SnapshotArray(int length) { // constructor
+        map = new HashMap[length];
+        for(int i=0;i<length;i++){
+            map[i] = new HashMap<>();
+            map[i].put(0,0);
+        }
+    }
+    
+    public void set(int index, int val) {
+        map[index].put(sid, val);
+    }
+    
+    public int snap() {
+        return sid++;
+    }
+    
+    public int get(int index, int snap_id) {
+        while(snap_id >= 0 && !map[index].containsKey(snap_id))
+            snap_id--;
+            if(snap_id ==-1){
+             return 0;}
+        return map[index].get(snap_id);
+
+    }
+}
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray obj = new SnapshotArray(length);
+ * obj.set(index,val);
+ * int param_2 = obj.snap();
+ * int param_3 = obj.get(index,snap_id);
+ */
