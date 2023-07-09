084f66b278248f6ce0d510eacb8127d0b9e9025a
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-07-09 12:16:12 +0530

    Added solution - LeetHub

---
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the smallest positive number missing from the array.
+    int missingNumber(int arr[], int n) 
+    { 
+        // Your code here
+        sort(arr, arr+n);
+        int j=1;
+        bool ansF = false;
+        for(int i=0; i<n; i++){
+            if(arr[i] > 0 && arr[i] == j){
+                j++;
+            }else if(arr[i] == arr[i-1] || arr[i] <=0){
+                continue;
+            }else  return j;
+        }
+        return j;
+    } 
+};
+
+//{ Driver Code Starts.
+
+int missingNumber(int arr[], int n);
+
+int main() { 
+    
+    //taking testcases
+    int t;
+    cin>>t;
+    while(t--){
+        
+        //input number n
+        int n;
+        cin>>n;
+        int arr[n];
+        
+        //adding elements to the array
+        for(int i=0; i<n; i++)cin>>arr[i];
+        
+        Solution ob;
+        //calling missingNumber()
+        cout<<ob.missingNumber(arr, n)<<endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
