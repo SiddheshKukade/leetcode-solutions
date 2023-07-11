5aee79100f01bea4aad56b9ec78cbd1e2a4777e3
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-07-11 23:23:19 +0530

    Time: 11 ms (91.07%), Space: 10.2 MB (56.19%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+           vector<pair<int,int>> v;
+        vector<string> ans(score.size());
+        for(int i=0;i<score.size();i++){
+            v.push_back({score[i],i});
+        }
+        sort(v.begin(),v.end(),greater());
+        for(int i=0;i<v.size();i++){
+            if(i==0){
+                ans[v[i].second]="Gold Medal";
+            }
+            else if(i==1){
+                ans[v[i].second]="Silver Medal";
+            }
+            else if(i==2){
+                ans[v[i].second]="Bronze Medal";
+            }
+            else{
+                ans[v[i].second]=to_string(i+1);
+            }
+        }
+        return ans;
+    }
+};
