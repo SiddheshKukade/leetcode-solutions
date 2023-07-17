1a49aaeefbc51dab03f031209f531866cac1d76d
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-07-17 12:25:32 +0530

    Time: 6 ms (32.33%), Space: 43.6 MB (49.94%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -12,7 +12,7 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
     Stack<Integer> stack1=  new Stack<>();
     Stack<Integer> stack2 = new Stack<>();
-    
+    //push both of the link list in stack to make them in reverse
     while(l1 != null){
         stack1.add(l1.val);
         l1= l1.next;
@@ -22,10 +22,11 @@ class Solution {
         l2= l2.next;
     }
         
+    // carry var and a temp node as result
     int carry=0;
     ListNode result =null;
     
-    while(!stack1.isEmpty() || !stack2.isEmpty() ){
+    while(!stack1.isEmpty() || !stack2.isEmpty() ){ // untill both finish
            int a=0,b=0;
             if(!stack1.isEmpty()){
                 a= stack1.pop(); 
@@ -34,17 +35,17 @@ class Solution {
                 b = stack2.pop();
             }
             int total = a+b+carry;
-            ListNode temp = new ListNode(total %10);
-            carry = total/10;
-            if(result == null){
-                result = temp;
+            ListNode temp = new ListNode(total %10); /// % 10 to remove the carry part
+            carry = total/10; /// getting carry by /10
+            if(result == null){// no prev result
+                result = temp; 
                 
-            }else{
-                temp.next = result;
-                result = temp;
+            }else{ // result already present 
+                temp.next = result; // save result in next node
+                result = temp;  // reinitialze result with temp
             }
         }
-        if(carry != 0){
+        if(carry != 0){  /// if carry exists in the end for ex 999 + 1 creates carry in end
             ListNode temp = new ListNode(carry);
             temp.next= result ; result = temp; 
         }
