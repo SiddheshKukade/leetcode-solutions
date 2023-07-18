9669b66033175ac30912f910aaf5ff6bdfc5e8a5
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-07-18 23:18:16 +0530

    Time: 500 ms (65.02%), Space: 178.5 MB (28.59%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,69 @@
+class LRUCache {
+public:
+    class node{ // doubly Linked list
+        public:
+        int key, val ; 
+        node* next ;
+        node* prev; 
+        node(int _key , int _val){
+            key = _key;
+            val = _val;
+        }
+    };
+    //dummy values
+    node * head = new node(-1 , -1 );
+    node * tail = new node(-1 , -1 );
+    
+    unordered_map<int , node* > m;
+    int cap ;
+    LRUCache(int capacity) {
+       cap = capacity;
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if(m.find(key) != m.end()){
+            node* resnode = m[key];
+            int res = resnode->val;
+           
+            //placing on a new address
+             m.erase(key);
+            deletenode(resnode);
+            addnode(resnode);
+            m[key] = head->next;
+            return res;
+        } 
+        // if key doesn't exists
+        return -1 ;
+    }
+    void put(int key, int value) {
+       if(m.find(key) != m.end()) {  // if the key exists in the hashmap 
+       //deleting current copy
+        node* existingnode = m[key];
+        m.erase(key);
+        deletenode(existingnode);
+       }   
+        if (m.size() == cap ){ // if capcity is full
+            //deleting the last one 
+           m.erase(tail->prev->key);
+           deletenode(tail->prev);
+        }
+        addnode(new node(key ,value)); // add node to head->next
+        m[key] = head->next;
+    }
+    void deletenode(node* delnode){
+        node* delprev = delnode->prev ; 
+        node* delnext = delnode->next;
+        delprev->next = delnext ;  // skipping current node from the list
+        delnext->prev= delprev;
+    } 
+     void addnode(node* newnode){
+         node* temp  = head->next; //temp is first element after head
+         newnode->next =  temp; //make the first element second 
+         newnode->prev = head; // attach newnode with head
+         head->next= newnode; //attach head with new node
+         temp->prev =newnode;// attach previois first prev as newnode
+     } 
+    
+};
