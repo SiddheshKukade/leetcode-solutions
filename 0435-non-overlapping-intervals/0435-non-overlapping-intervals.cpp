2daaa2e1b2886738ee4320f512db3fe9583ac43d
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-07-20 12:06:46 +0530

    Time: 478 ms (70.59%), Space: 89.7 MB (58.56%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    static bool cmp(vector<int>& a, vector<int>& b){
-        return a[1] < b[1];
-    }
+ 
 
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        int previous = 0;
         int n = intervals.size();
-        sort(intervals.begin(), intervals.end(), cmp);
-
-        int prev = 0;
-        int count = 1;
-
-        for(int i = 1; i < n; i++){
-            if(intervals[i][0] >= intervals[prev][1]){
-                prev = i;
-                count++;
+        int ans=0;
+        for(int current = 1; current<n; current++){
+            if(intervals[current][0] < intervals[previous][1]){
+                ans++;
+                if(intervals[current][1] <=intervals[previous][1]){
+                    //delete the bigger interval
+                    previous = current; 
+                }
+            }else{
+                previous=current;
             }
         }
-        return n - count;
+        return ans;
     }
 };
