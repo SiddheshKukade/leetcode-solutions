90c9bd3b17b92ae1c68a69d490022f983e209e5d
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-07-26 13:14:07 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (41.11%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,58 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+class Solution {
+    void dfs(Node* curr, Node* node, vector<Node*> &visited){
+        visited[node->val] = node;
+        for(auto ele: curr->neighbors){
+            if(visited[ele->val] == NULL){
+                Node* newnode  = new Node(ele->val);
+                (node->neighbors).push_back(newnode);
+                dfs(ele, newnode, visited);
+            }else{
+                (node->neighbors).push_back(visited[ele->val]);
+                }
+        }
+    }
+public:
+    
+    Node* cloneGraph(Node* node) {
+        if(node == NULL)
+            return NULL;
+        vector<Node*> visited(1000, NULL);
+        Node* copy = new Node(node->val);
+        visited[node->val] = copy;
+        
+        for(auto curr: node->neighbors)
+        {
+            if(visited[curr->val] == NULL){
+            Node *newnode = new Node(curr->val);
+            (copy->neighbors).push_back(newnode);  // also updating the adjacency list here
+            dfs(curr, newnode, visited);
+                
+            }else{
+                (copy->neighbors).push_back(visited[curr->val]);
+            }           
+        }
+        
+        return copy;
+        
+    }
+};
