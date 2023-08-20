7db9227e22dd4a005e245cdd852f677b918c48d6
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-08-20 17:11:35 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -8,46 +8,34 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
-   int binarySearch(int a[],int n,int e)
-        {
-            int beg=0,end=n-1,mid=(beg+(end-beg)/2);
-            while(beg<=end)
-            {
-             mid=(beg+(end-beg)/2);
-             if(a[mid]==e)
-             return mid;
-             else if(a[mid]<e)
-             beg=mid+1;
-             else
-             end=mid-1;
-            }
-            return -1;
-        }
+
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
 	int count(int arr[], int n, int x) {
    int s = 0;
 	    int e = n-1;
-	    
+	    //norm bs condition
 	    while(s <= e){
+	        ///normal  mid from start to end
 	        int mid = s+(e-s)/2;
-	        
+	        ///if start or end is the same number
 	        if(arr[s] == x && arr[e] == x){
+	            //return count of all nos between them
 	            return e-s+1;
 	        }
-	        if(arr[mid] == x){
-	            if(arr[s] != x){
-	                s++;
+	        if(arr[mid] == x){ /// if mid is the no
+	            if(arr[s] != x){ /// if start is not the no
+	                s++; // increase  start
 	            }
-	            if(arr[e] != x){
-	                e--;
+	            if(arr[e] != x){ // end not the no 
+	                e--;// reduce range (decrease end)
 	            }
 	        }
-	        else if(arr[mid] > x){
-	            e = mid-1;
+	        else if(arr[mid] > x){ // if mid more than x 
+	            e = mid-1; // go to left
 	        }
 	        else if(arr[mid] < x){
-	            s = mid+1;
+	            s = mid+1; // go to right
 	        }
 	    }
 	    return 0;
