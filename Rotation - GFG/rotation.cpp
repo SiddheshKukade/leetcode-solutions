7c2ee184f26d174bf8e3d5ca1a129ff3516beac3
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-09-20 16:03:09 +0530

    Added solution - LeetHub

---
+++ Rotation - GFG/rotation.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int findKRotation(int arr[], int n) {
+	    // code here Given an ascending sorted rotated array Arr of distinct integers of size N. The array is right rotated K times. Find the value of K
+	     //complete the function here
+        for(int i=1; i<n ;i++){
+            if(arr[i-1] > arr[i]){
+                return i;
+            }
+        }
+        return 0;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findKRotation(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
