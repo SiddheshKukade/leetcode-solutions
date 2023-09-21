ed144f75204a412156978b789149754ba43a2e28
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-09-21 23:32:23 +0530

    Added solution - LeetHub

---
+++ Stickler Thief - GFG/stickler-thief.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:  
+    
+    def FindMaxSum(self,a, n):
+        # code here
+        dp = [0 for i in range(n)]
+        dp[0] = a[0]
+        dp[1] = max(a[0], a[1])
+        for i in range(2, n): dp[i] = max(a[i] + dp[i-2], dp[i-1])
+        return dp[n-1]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+sys.setrecursionlimit(10**6)
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        a = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(ob.FindMaxSum(a,n))
+# } Driver Code Ends
