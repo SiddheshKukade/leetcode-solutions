f064b0fd995d8905860bd421c83879827ff721b4
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-09-30 14:34:00 +0530

    Added solution - LeetHub

---
+++ Remove duplicates from a sorted doubly linked list - GFG/remove-duplicates-from-a-sorted-doubly-linked-list.cpp
@@ -0,0 +1,186 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+
+Node *newNode(int data)
+{
+    Node *temp=new Node(data);
+    
+    return temp;
+}
+
+
+
+
+void displayList(Node *head)
+{
+    //Head to Tail
+    while(head->next)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<head->data;
+    
+    
+    
+}
+
+
+int getLength(Node * head)
+{
+    Node *temp=head;
+    
+    int count=0;
+    while(temp->next!=head)
+    {
+        count++;
+        temp=temp->next;
+    }
+    return count+1;
+}
+
+
+
+
+bool verify(Node* head)
+{
+    int fl=0;
+    int bl=0;
+    
+    Node *temp=head;
+    
+    while(temp->next)
+    {
+        temp=temp->next;
+        fl++;
+    }
+    
+    while(temp->prev)
+    {
+        temp=temp->prev;
+        bl++;
+    }
+    
+    return fl==bl;
+}
+
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+*/
+
+class Solution
+{
+public:
+
+    Node * removeDuplicates(struct Node *head)
+    {
+        // Your code here
+        
+        // if(head == NULL || head->next == NULL) return head;
+        // Node* prev=head;
+        // Node* curr=head->next;
+        // while(curr->next != NULL){
+            
+        //     if(curr->data == prev->data){
+        //         curr->next->prev = prev;
+        //         prev->next= curr->next;
+        //     }else{
+        //         prev = prev->next;
+        //     }
+            
+        //     curr = curr->next;
+        // }
+        // if(curr->data == prev->data){
+        //     prev->next= NULL;
+        // }
+        // return curr;
+         Node* temp=head;
+        
+        if(head==NULL || head->next==NULL)return head;
+        
+        while(temp->next)
+        {
+            if(temp->data==temp->next->data)
+            {
+                temp->next=temp->next->next;
+                
+                if(temp->next)
+                temp->next->prev=temp;
+
+            }
+            else
+            {
+                temp=temp->next;
+            }
+        }
+        return head;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    Node *head=NULL, *tail=NULL;
+        int x;
+	    cin>>x;
+	    head = newNode(x);
+	    tail = head;
+	    
+	    for(int i=0;i<n - 1;i++)
+	    {
+	        cin>>x;
+	        Node* temp=newNode(x);
+	        tail->next=temp;
+	        temp->prev= tail;
+	        tail = temp;
+	    }
+	    Solution obj;
+	    head=obj.removeDuplicates(head);
+	    
+	    
+	    if(verify(head))
+	    displayList(head);
+	    else
+	    cout<<"Your pointers are not correctly connected";
+ 
+	    cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
