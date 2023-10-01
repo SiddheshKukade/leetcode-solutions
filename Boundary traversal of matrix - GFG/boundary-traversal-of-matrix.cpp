f8b19e9c23ec4725d39c63fd6a58259552c0c99b
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-10-01 23:47:56 +0530

    Added solution - LeetHub

---
+++ Boundary traversal of matrix - GFG/boundary-traversal-of-matrix.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+class Solution
+{   
+    public:
+    //Function to return list of integers that form the boundary 
+    //traversal of the matrix in a clockwise manner.
+    vector<int> boundaryTraversal(vector<vector<int> > matrix, int n, int m) 
+    {
+        // code here
+        vector<int> ans;
+        int ii=0, jj =0;
+        for(int j=0; j<m;j++){
+            ans.push_back(matrix[ii][j]);
+        }
+        jj = m-1;
+        for(int i=1; i<n;i++){
+            ans.push_back(matrix[i][jj]);
+        }
+           if (n == 1)
+    {
+        return ans;
+    }
+        ii = n-1;
+        for(int j=m-2; j>=0 ;j--){
+            ans.push_back(matrix[ii][j]);
+        }if (m == 1)
+    {
+        return ans;
+    }
+        jj=0;
+          for(int i=n-2; i>=1 ;i--){
+            ans.push_back(matrix[i][jj]);
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n,m;
+        cin>>n>>m;
+        vector<vector<int> > matrix(n); 
+
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(m, 0);
+            for( int j=0; j<m; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.boundaryTraversal(matrix, n, m);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
